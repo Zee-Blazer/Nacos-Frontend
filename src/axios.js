2a557dca6b15c8abe6d41f183bfd51b5adc0a5c8
Ganiyu Bolaji <77048751+Zee-Blazer@users.noreply.github.com>
2023-09-28 11:31:42 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: `https://sleepy-earth-65067.herokuapp.com/`
+    baseURL: `https://nacos-backend.onrender.com/`
 })
 
 export default instance
